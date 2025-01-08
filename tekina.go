6084a2b1d4fe7b9d0e14905c3a85017c166701cd
Geek-Tekina <tpbc1717@gmail.com>
2025-01-08 14:05:03 +0530

    Added aboutMe fnc

--- tekina.go
+++ tekina.go
@@ -1,5 +1,10 @@
 package tekina
 
+import "fmt"
+
+func aboutMe() {
+	fmt.Println("Hey this is Aniket, the author of this go module")
+}
 func main() {
 	// This is my first go package which is going to github !!
 }
